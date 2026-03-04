2b48985d96118031f6efefa90d652c8be8669742
ganeshreddy1524 <ganeshreddykandi@gmail.com>
2026-03-04 15:08:16 +0530

    url updated

--- task-ui/src/environments/environment.prod.ts
+++ task-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://44.195.1.174:8080',
-  fileApiUrl: 'https://hk327mcsu7.execute-api.us-east-1.amazonaws.com/prod'
+  apiUrl: 'http://3.109.62.7:8080',
+
 };
